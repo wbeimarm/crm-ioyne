038f4137890b52ff6bb42e4f9eef923dc3134e82
wbeimarm <wmarmonsa@gmail.com>
2025-04-29 17:57:07 -0500

    feat: agregando rutas de usuario al servidor y proteger .env

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 const expres = require('express');
 const cors = require('cors');
+const app = expres();
+
+// Cargamos variables de entorno desde el archivo .env
 require('dotenv').config;
 
-const app = expres();
+//Importamos las rutas de usuarios
+const usuarioRoutes = require('./routes/usuarioRoutes');
+
+// Activamos middlewares
 app.use(cors());
 app.use(expres.json());
 
+//Ruta base
 app.get('/', (req, res) => {
     res.send('¡Servidor CRM IOYNE funcionando!')
 });
 
+
+//Usamos las rutas de usuarios
+app.use('/api/usuarios', usuarioRoutes);
+
+//Encendemos el servidor en el puerto configurado
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost: ${PORT}`)
